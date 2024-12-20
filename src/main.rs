a9e256abc9bf47c1049d044c582a1e4c5ea6f74d
mejxe <mlaskowski707@gmail.com>
2024-12-20 14:07:28 +0100

    Test regex

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 use std::fs::rename;
 
 #[derive(Debug)]
-enum Error {
-    MissingArguments(String),
+enum AllError {
+    MissingArguments,
     ReadError(std::io::Error),
+    NoMatches(()),
 }
-fn main() -> Result<(), Error> {
-    let from = std::env::args().nth(1).ok_or(Error::MissingArguments("Missing args".to_string()))?;
-    let to = std::env::args().nth(2).ok_or(Error::MissingArguments("Missing args".to_string()))?;
+impl From<std::io::Error> for AllError{
+    fn from(value: std::io::Error) -> Self {
+        AllError::ReadError(value)
+    }
+}
+        
+fn main() -> Result<(), AllError> {
+    let main_regex = regex::RegexBuilder::new(r"(?:ep|episode)?\s*(\d{1,3})[^0-9]").case_insensitive(true).build().unwrap();
+    let current_folder = std::fs::read_dir("./data").unwrap();
+    let from = std::env::args().nth(1).ok_or(AllError::MissingArguments)?;
+    let to = std::env::args().nth(2).ok_or(AllError::MissingArguments)?;
     println!("{from}, {to}");
-    rename(from, to).map_err(|err|Error::ReadError(err))?;
+    for file in current_folder {
+        let test = main_regex.find(&file.unwrap().file_name().to_str().unwrap()).ok_or(AllError::NoMatches(()))?.as_str().to_string();
+        println!("{}",&test);
+    }
+    rename(from, to)?;
     Ok(())
 
 }
